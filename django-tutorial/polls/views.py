c5cdec3ce0c4bf1f96ec03aecd95c9eb8571c6d2
baucuenca <jnb.cuenca1507@gmail.com>
2025-04-15 11:16:15 -0300

    Parte 3.2
    Implementacion de views index y detail. Crecion de templates.

--- django-tutorial/polls/views.py
+++ django-tutorial/polls/views.py
@@ -1,13 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_list_or_404
+from django.http import HttpResponse, Http404
+# from django.template import loader
+from .models import Question, Choice
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("you r at the polls index")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
-    return HttpResponse(f"you r looking at question {question_id}")
+    question = get_list_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
     return HttpResponse(f"you r looking at the results of question {question_id}")
