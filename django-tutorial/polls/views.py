91afaf7ec9cb34ffcbbb257180a36c0c3f73997d
baucuenca <jnb.cuenca1507@gmail.com>
2025-04-15 10:34:26 -0300

    Parte 3.1
    Creacion de nuevas views.

--- django-tutorial/polls/views.py
+++ django-tutorial/polls/views.py
@@ -4,4 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("You r at the polls index")
+    return HttpResponse("you r at the polls index")
+
+def detail(request, question_id):
+    return HttpResponse(f"you r looking at question {question_id}")
+
+def results(request, question_id):
+    return HttpResponse(f"you r looking at the results of question {question_id}")
+
+def vote(request, question_id):
+    return HttpResponse(f"you r voting on question {question_id}")
