03204d40d8e4284d5b04c0be9209b59f51bdaefb
baucuenca <jnb.cuenca1507@gmail.com>
2025-04-15 14:47:58 -0300

    Parte 4.1
    Implementacion de un formulario basico.

--- django-tutorial/polls/views.py
+++ django-tutorial/polls/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, HttpResponseRedirect
 # from django.template import loader
 from .models import Question, Choice
+from django.urls import reverse
+from django.db.models import F #operaciones de referencia a campos de la base de datos
 
 # Create your views here.
 
@@ -17,7 +19,25 @@ def detail(request, question_id):
     return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
-    return HttpResponse(f"you r looking at the results of question {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 def vote(request, question_id):
-    return HttpResponse(f"you r voting on question {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # Volver a mostrar el form de la pregunta para votar
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1 # se realiza el incremento en la base de datos
+        selected_choice.save()
+        # Siempre redirigir después de procesar el POST para evitar el reenvío del formulario al actualizar la página.
+    return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
